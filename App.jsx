c6c2b7d866ab57ae214ba1e274caaec9042cbf2f
junaidjahan706 <150780751+junaidjahan706@users.noreply.github.com>
2023-11-17 12:09:30 -0800

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,80 @@
+import "./App.css";
+
+function Cv() {
+  return (
+    <>
+      <div className="container">
+        <div className="sidebar">
+          <div className="pic"></div>
+          <h1>Contact</h1>
+          <h4>Phone:</h4>
+          <h4>03040035773</h4>
+          <h4>03126575055</h4>
+          <h4>Email:</h4>
+          <h4>junaidjahan706@gmail.com</h4>
+          <h4>Adress:</h4>
+          <h4>Basti Kamboah abad Chowk Bahadur pur Rahim Yar Khan</h4>
+          <h1>Skills</h1>
+            <h4>Html</h4>
+            <h4>CSS</h4>
+            <h4>Bootstrap</h4>
+            <h4>Javascript</h4>
+            <h1>Hobby</h1>
+            <h4>Holy Quran</h4>
+            <h4>Cricket</h4>
+            <h4>Football</h4>
+            <h4>Reading Books</h4>
+            <h4>Writting</h4>
+          <div className="contact">
+           
+          </div>
+          {/* <div className="certificate">
+            <h1>Certificate</h1><br></br>
+            <h4>Best performer in skyrass software house</h4><br></br>
+            <h4>Best performer in syntax software house</h4>
+          </div> */}
+          <div className="skills">
+           
+          
+          </div>
+          <div className="hobby">
+            
+          </div>
+        </div>
+        <div className="con">
+          <h1 className = "head" style={{color:"white",backgroundColor:"black",}}>About</h1>
+          <p>A passionate dedicated Web devloper with a keen ability . I bring 1 year of experience in Sky rass, where I have consistently best performer. My approach is characterized by specific qualities or characteristics, e.g., adaptability, strategic thinking, problem-solving.
+
+I am known for my  attention to detail, strong communication, leadership and have a proven track record of  achievements or projects. My commitment to specific values or principles guides my work, and I thrive in dynamic environments where I can contribute to  driving innovation, achieving operational excellence.
+
+In my previous roles at Skyrass, . These experiences have honed my ability to mention another key skill or strength, and I am eager to bring this expertise to a new challenge with Target Company. I am confident in my ability to contribute to the prospective employer.
+
+I am a lifelong learner, multilingual, and I am excited about the prospect of contributing to a dynamic team, driving business growth. Let's connect to discuss how my skills and experiences align with the goals of Target Company.
+
+Feel free to tailor this text to better reflect your unique background, skills, and aspirations. It's important to strike a balance between being concise and providing enough information to capture the reader's interest.</p>
+       
+       <h1 className = "head" style={{color:"white",backgroundColor:"black",}}>Education</h1>
+<h4>Matric:</h4> 
+<p>Studied at Govt.Boys Higher Sceondry School Chowk Bahadur pur RYK</p>  
+<h5>Marks</h5>
+<p>822 with percentage of 74% </p>    
+ <h4>Inter:</h4>
+ <p>Studied at Superior group of Colleges RYK</p> 
+ <h5>Marks</h5>
+<p>581 with percentage of 52% </p> 
+ <h4>BS Software Engineering:</h4> 
+ <p>Studied At Khawaja Fareed UniverstyRahim Yar Khan</p>    
+ <h1 className = "head" style={{color:"white",backgroundColor:"black",}}>Certificate</h1>
+            <h4>*Best performer in skyrass software house</h4>
+            <h4>*Best performer in syntax software house</h4>
+            <h1 className = "head lg" style={{color:"white",backgroundColor:"black",}}>language</h1>
+            <h4>*English</h4>
+            <h4>*Urdu</h4>
+            <h4>*Saraki</h4>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cv;
